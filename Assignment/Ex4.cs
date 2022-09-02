f9db042e2fa505684a4c6d9ca7bb4f63cb3d5170
Supriya K J <86228362+321supriya@users.noreply.github.com>
2022-09-02 15:01:53 +0530

    Add files via upload

---
+++ Assignment/Ex4.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Assignmnet
+{
+    class sum
+    {
+
+        public static void Main()
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 6 };
+            int even = 0, odd = 0;
+
+            // Loop to find even, odd sum
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i % 2 == 0)
+                    even += arr[i];
+                else
+                    odd += arr[i];
+            }
+
+            Console.WriteLine("Even index positions"
+                                 + " sum: " + even);
+
+            Console.WriteLine("Odd index positions "
+                                   + "sum: " + odd);
+        }
+    }
+}
---
+++ Assignment/Ex5.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Assignment
+{
+    class Reverse
+    {
+        public static string reverseByWords(string Input)
+        {
+            char[] charArray = Input.ToCharArray();
+            string word = String.Empty;
+            for (int i = charArray.Length - 1; i > -1; i--)
+            {
+                word += charArray[i];
+            }
+            return word;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(reverseByWords("Hello world"));
+            Console.WriteLine(reverseByWords("My name Supriya"));
+            
+        }
+    }
+}
