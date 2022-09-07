567b3a1163d31400442a376f3d43408fb8458c71
Supriya K J <86228362+321supriya@users.noreply.github.com>
2022-09-07 21:53:52 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MathLibrary
+{
+    public class Class1
+    {
+        public float Addition(float a, float b)
+        {
+            return a - b;
+        }
+        public float Substract(float a, float b)
+        {
+            return a - b;
+        }
+        public float Multiply(float a, float b)
+        {
+            return a * b;
+        }
+        public float Divide(float a, float b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ Mathfunc.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MathLibrary; // Importing Math Library
+
+namespace MathApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare class from MathLibrary.dll
+            Class1 math = new Class1();
+
+            // Declare variables and use methods from MathLibrary
+            float addition = math.Addition(5, 2);
+            
+            Console.WriteLine("The sum of two numbers is: "+ addition);
+
+            Console.ReadLine();
+        }
+    }
+}
